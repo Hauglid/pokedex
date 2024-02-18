a1a5d64050dce6e68d2609e5ca17dff8d530126f
Karl Thomas Hauglid <kthauglid@gmail.com>
2024-02-18 14:38:06 +0100

    Refactor moves component to use key prop for each move

--- components/pokemon.[name]/ui/moves.tsx
+++ components/pokemon.[name]/ui/moves.tsx
@@ -6,10 +6,11 @@ export function Moves({ moves }: { moves: Move[] }) {
   return (
     <View className="flex flex-row flex-wrap justify-center p-4 gap-4 ">
       {moves.map((move) => (
-        <View className="rounded-full border shadow-md bg-white p-2">
-          <Text key={move.move.name} className="px-2 ">
-            {move.move.name.toUpperCase()}
-          </Text>
+        <View
+          key={move.move.name}
+          className="rounded-full border shadow-md bg-white p-2"
+        >
+          <Text className="px-2 text-lg">{move.move.name.toUpperCase()}</Text>
         </View>
       ))}
     </View>
